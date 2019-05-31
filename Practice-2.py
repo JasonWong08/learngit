c5cbb521410f6ddd7aa1d2afc75d54569022ef84
JasonWong08 <jarodwang1981@gmail.com>
2019-05-31 17:50:38 +0800

    add Practice-2.py

---
+++ Practice-2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: wangjf
+date: 2019/5/31 17:20
+desc:
+"""
+from collections import Iterable
+from functools import reduce
+
+
+def add(x, y):
+    return x + y
+
+
+reduce(add, [1, 3, 5, 7, 9])
+isinstance([], Iterable)
